c439789285d923b83067c862f3a2f397a29a1c11
Daniel Long <danielwlong618@gmail.com>
2023-09-05 21:47:25 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat (name) {
+    cats.push('Ralph')
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat (name) {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat (name) {
+    cats.shift()
+}
+
+function appendCat (name) {
+    return appendCat = [...cats, 'Broom']
+}
+
+function prependCat (name) {
+    return prependCat = ['Arnold', ...cats]
+}
+
+function removeLastCat (name) {
+    return removeLastCat = cats.slice(0,2)
+}
+
+function removeFirstCat (name) {
+    return removeFirstCat = cats.slice(1)
+}
